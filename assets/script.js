64d55bf3f9a11c958cb6fd928317c3a68e74c9df
Kim Jevon <aotfqg4h@students.codeinstitute.net>
2024-12-31 10:08:23 +0000

    added script.js - utilized to adjust email sign up form in footer. sign up button changes to thanks

---
+++ assets/script.js
@@ -0,0 +1,35 @@
+const emailInput = document.getElementById('emailInput');
+const submitButton = document.getElementById('submitButton');
+const thankYouMessage = document.getElementById('thankYouMessage');
+const form = document.getElementById('emailForm');
+
+// Enable the submit button when a valid email is entered
+emailInput.addEventListener('input', function () {
+    if (emailInput.validity.valid) {
+        submitButton.disabled = false;
+    } else {
+        submitButton.disabled = true;
+    }
+});
+
+// Handle form submission
+form.addEventListener('submit', function (event) {
+    event.preventDefault(); 
+
+    submitButton.textContent = 'THANKS!';
+    
+    submitButton.disabled = true;
+    
+    thankYouMessage.style.display = 'block';
+    
+    emailInput.value = '';
+});
+
+
+
+
+
+
+
+
+
