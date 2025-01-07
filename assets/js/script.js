98578aa0733e700a723018855ea0004d3a5b09ae
Kim Jevon <aotfqg4h@students.codeinstitute.net>
2025-01-07 12:58:54 +0000

    need to debug button styles on carousel overlay

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,13 @@
+
+
 // Footer Form Logic (Email Sign-Up Form)
 
-// Accessing elements for the footer form
+// accessing elements for the footer form
 const emailInput = document.getElementById('emailInput');
 const submitButton = document.getElementById('submitButton');
 const emailForm = document.getElementById('emailForm');
 
-// Enable/Disable Submit Button based on Email Validity
+// enable/disable submit button 
 emailInput.addEventListener('input', function () {
     if (emailInput.validity.valid) {
         submitButton.disabled = false;
@@ -14,36 +16,37 @@ emailInput.addEventListener('input', function () {
     }
 });
 
-// Handling form submission (for the footer form)
+// handling footer form submission 
 emailForm.addEventListener('submit', function (event) {
-    event.preventDefault(); // Prevent the form from submitting
+    event.preventDefault(); // prevent the form from submitting
 
-    submitButton.textContent = 'THANKS!'; // Change button text
+    submitButton.textContent = 'THANKS!'; // change button text
 
-    // Clear the input field and change its placeholder
+// Clear the input field and change its placeholder
     emailInput.value = '';
     emailInput.placeholder = 'ALL DONE...';
 });
 
-// Contact Form Logic
+// contact form logic
 
-// Accessing elements for the contact form
+// accessing elements for the contact form
 const contactForm = document.querySelector('.contactForm');
 const contactHeading = document.querySelector('.contactHeader h1');
 const formFields = document.getElementById('formFields');
 const thankYouMessage = document.getElementById('thankYouMessage');
 const socialIcons = document.getElementById('socialIcons');
 
-// Handling form submission (for the contact form)
+// handling form submission (for the contact form)
 contactForm.addEventListener('submit', function (event) {
     event.preventDefault(); // Prevent the form from submitting
 
-    // Change heading text and hide the form fields
+    // change heading text and hide the form fields
     contactHeading.textContent = 'SPEAK SOON!';
     formFields.classList.add('d-none');
 
-    // Show the thank-you message
+    // show the thank-you message
     thankYouMessage.classList.remove('d-none');
 
     socialIcons.classList.remove('d-none');
 });
+
